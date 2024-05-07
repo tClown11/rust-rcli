12505927f3d1d1dfc7a3f86bed9d831eb29dec7a
Clown <38233074+tClown11@users.noreply.github.com>
2024-05-07 21:20:27 +0800

    feature: clap-parse complate (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::path::Path;
+
+// target : rcli csv -i input.csv -o output.json --header -d ','
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+}
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about=None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, value_parser = verify_input_file)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")] // "output.json".into()
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
+fn verify_input_file(filename: &str) -> Result<String, String> {
+    if Path::new(filename).exists() {
+        Ok(filename.into())
+    } else {
+        Err("File does not exist".into())
+    }
 }

b6c874022a295fea9bfb8004ac03dfddd8c720f1
Clown <38233074+tClown11@users.noreply.github.com>
2024-05-08 21:47:47 +0800

    feat:support generic csv to json and yaml output (#5)

--- src/opts.rs
+++ src/opts.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::path::Path;
+use std::{
+    fmt::{self},
+    path::Path,
+    str::FromStr,
+};
 
 #[derive(Debug, Parser)]
 #[command(name = "rcli", version, author, about, long_about=None)]
@@ -14,25 +18,68 @@ pub enum SubCommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+    //Toml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")] // "output.json".into()
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
 
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
 
     #[arg(long, default_value_t = true)]
     pub header: bool,
+
+    #[arg(long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
 }
 
-fn verify_input_file(filename: &str) -> Result<String, String> {
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
     if Path::new(filename).exists() {
         Ok(filename.into())
     } else {
-        Err("File does not exist".into())
+        Err("File does not exist")
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            // OutputFormat::Toml => "toml",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            //"toml" => Ok(OutputFormat::Toml),
+            v => anyhow::bail!("Unsupported format: {}", v),
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }

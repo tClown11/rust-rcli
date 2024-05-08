3d3ba06a8b3227498e1eb554d592be4217abaf86
clown <tj1211keynote@outlook.com>
2024-05-08 21:46:54 +0800

    feat:support generic csv to json and yaml output

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ use rcli::{process_csv, Opts, SubCommand};
 fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     match opts.cmd {
-        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+        // 读取 csv 文件
+        SubCommand::Csv(opts) => {
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?;
+        }
     }
     Ok(())
 }
--- src/process.rs
+++ src/process.rs
@@ -4,6 +4,8 @@ use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
 
+use crate::opts::OutputFormat;
+
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Player {
@@ -16,15 +18,32 @@ pub struct Player {
     pub kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
-    //let mut ret: Vec<Player> = Vec::new();
-    let records = reader
-        .deserialize()
-        .map(|record| record.unwrap())
-        .collect::<Vec<Player>>();
+    let mut ret = Vec::with_capacity(128);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        // headers.iter() -> 使用 headers 的迭代器
+        // record.iter() -> 使用 record 的迭代器
+        // zip() -> 将两个迭代器合并为一个元组的迭代器 [(header, record), ..]
+        // collect::<Value>() -> 将元组的迭代器转换为 JSON Value
+        let json_value = headers
+            .iter()
+            .zip(record.iter())
+            .collect::<serde_json::Value>();
 
-    let json = serde_json::to_string_pretty(&records)?;
+        ret.push(json_value);
+    }
+
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        // 暂不支持 toml
+        //OutputFormat::Toml => toml::to_string(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+
+    let json = serde_json::to_string_pretty(&content)?;
     fs::write(output, json)?; // => ()
     Ok(())
 }
